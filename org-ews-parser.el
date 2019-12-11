59c71e1f69f4be42ffb238dc43d86b182691d2e6
Alex Egger <alex.egger96@gmail.com>
2019-12-11 22:24:51 +0100

    Update parsing functions

--- org-ews-parser.el
+++ org-ews-parser.el
@@ -3,137 +3,123 @@
 ;;; Commentary:
 
 ;;; Code:
+(require 'dom)
+(require 'parse-time)
 
-(defvar org-ews-default-subject "Untitled Event")
-(defvar org-ews-default-location "Unspecified Location")
+(defconst org-ews-default-subject "Untitled Event"
+  "The default subject for a calendar entry that does not have a valid one.")
+
+(defconst org-ews-default-location "Unspecified Location"
+  "The default location for a calendar entry that does not have a valid one.")
+
+(defconst org-ews-default-legacy-free-busy-status :free
+  "The default legacy free busy status for a calendar entry that does not have a valid one.")
+
+(defconst org-ews-default-my-response-type :unknown
+  "The default my response type for a calendar entry that does not have a valid one.")
 
 (defvar org-ews--fields
-  `((:subject . '(:parser org-ews--parse-subject :tag 'Subject :required t :default ,org-ews-default-subject))
-   (:start-time . '(:parser org-ews--parse-start-time :tag 'Start :required t ))
-   (:end-time . '(:parser org-ews--parse-end-time :tag 'End :required t))
-   (:location . '(:parser org-ews--parse-location :tag 'Location :required nil :default ,org-ews-default-location))
-   (:legacy-free-busy-status . '(:parser org-ews--parse-legacy-free-busy-status :tag 'LegacyFreeBusyStatus :required nil))
-   (:item-type . '(:parser org-ews--parse-item-type :tag 'CalendarItemType :required nil))
-   (:organizer . '(:parser org-ews--parse-organizer :tag 'Organizer :required nil))))
+  `((:subject . '(:parser org-ews--parse-text-field :tag 'Subject :required t :default ,org-ews-default-subject))
+    (:start-time . '(:parser org-ews--parse-time-field :tag 'Start :required t))
+    (:end-time . '(:parser org-ews--parse-time-field :tag 'End :required t))
+    (:location . '(:parser org-ews--parse-text-field :tag 'Location :required nil :default ,org-ews-default-location))
+    (:legacy-free-busy-status . '(:parser org-ews--parse-legacy-free-busy-status :tag 'LegacyFreeBusyStatus :required nil :default ,org-ews-default-legacy-free-busy-status))
+    (:my-response-type . '(:parser org-ews--parse-my-response-type :tag 'MyResponseType :required nil :default ,org-ews-default-my-response-type))
+    (:item-type . '(:parser org-ews--parse-calendar-item-type :tag 'CalendarItemType :required nil))
+    (:organizer . '(:parser org-ews--parse-organizer :tag 'Organizer :required nil))))
 
-(defun org-ews--get-field-required (field)
-  "Return the `:required' property of FIELD.
-Field must exist in `org-ews--fields', otherwise nil is returned."
-  (plist-get (caddr (assq field org-ews--fields)) :required))
+(defmacro org-ews--get-field-property (prop)
+  "Define a function named `(concat \"org-ews--get-field-\" PROP)'.
+The function returns the respective property, when given a field from `org-ews--fields'."
+  `(defun ,(intern (concat "org-ews--get-field-" prop)) (field)
+     ,(concat "Return the `:" prop "' property of FIELD.
+Field must exist in `org-ews--fields', otherwise nil is returned.")
+     (plist-get (caddr (assq field org-ews--fields)) ,(intern (concat ":" prop)))))
 
-(defun org-ews--get-field-parser (field)
-  "Return the `:parser' property of FIELD.
-Field must exist in `org-ews--fields', otherwise nil is returned."
-  (plist-get (caddr (assq field org-ews--fields)) :parser))
-
-(defun org-ews--get-field-default (field)
-  "Return the `:default' property of FIELD.
-Field must exist in `org-ews--fields', otherwise nil is returned."
-  (plist-get (caddr (assq field org-ews--fields)) :default))
+(org-ews--get-field-property "parser")
+(org-ews--get-field-property "tag")
+(org-ews--get-field-property "required")
+(org-ews--get-field-property "default")
+(org-ews--get-field-property "empty")
 
 (defun org-ews--parse-field (dom field)
-  (ignore))
+  "Parse DOM and return the value associated with FIELD.
+FIELD should be a key from `org-ews--fields'."
+  (let ((value (funcall (org-ews--get-field-parser field)
+                        (dom-by-tag dom (org-ews--get-field-tag field)))))
+    (if (not (org-ews--get-field-empty field))
+        (unless (and (stringp value) (string= "" value)) value) value)))
 
-(defun org-ews--parse-response (response)
-  "Parse RESPONSE into a list of 'CalendarItem."
-  (with-temp-buffer
-    (insert response)
-    (let ((content (libxml-parse-xml-region (point-min) (point-max))))
-      (dom-by-tag content 'CalendarItem))))
+(defun org-ews--parse-text-field (dom)
+  "Parse DOM and return its text value."
+  (dom-text dom))
 
-(defun org-ews--parse-calendar-item (item-node)
-  "Parse ITEM-NODE into an assocation list."
-  (ignore))
+(defun org-ews--parse-boolean-field (dom)
+  "Parse DOM and return its text value as a boolean value.
+If the value is `true' or `True' t is returned, other values result in nil."
+  (let ((text (dom-text dom)))
+    (or (string= "true" text) (string= "True" text))))
 
-(defconst org-ews-subject-fallback "Untitled Event"
-  "The fallback subject for an event that does not have a title or whose title is the empty string.")
+(defun org-ews--parse-time-field (dom)
+  "Parse DOM and return its text value as a time string in `org-mode' format."
+  (format-time-string "%Ft%T%z" (parse-iso8601-time-string (dom-text dom))))
 
-(defun org-ews--parse-subject (node)
-  "Parse NODE and return its subject."
-  (org-ews--parse-simple-text-field (dom-by-tag node 'Name) org-ews-subject-fallback))
+(defun org-ews--parse-legacy-free-busy-status (dom)
+  "Parse DOM and return its text value as a symbol representing the legacy free busy status.
+Possible values for the legacy free busy status include:
+- `:free'
+- `:busy'
+- `:no-data'
+- `:out-of-office'
+- `:tentative'
+- `:working-elsewhere'
+The meaning of these values corresponds to their definition in the EWS documentation."
+  (let ((text (dom-text dom)))
+    (cond ((string= "Free" text) :free)
+          ((string= "Busy" text) :busy)
+          ((string= "NoData" text) :no-data)
+          ((string= "OOF" text) :out-of-office)
+          ((string= "Tentative" text) :tentative)
+          ((string= "WorkingElsewhere" text) :working-elsewhere)
+          (t nil))))
 
-(defun org-ews--parse-start-time (node)
-  "Parse NODE and return its starting time.
-Since there is no sane fallback value for this field we return nil upon error instead."
-  (org-ews--parse-simple-text-field (dom-by-tag node 'Start) nil))
+(defun org-ews--parse-calendar-item-type (dom)
+  "Parse DOM and return its text value as a symbol representing the calendar item type property.
+Possible values for the calendar item type include:
+- `:single'
+- `:occurence'
+- `:exception'
+- `:recurring-master'
+The meaning of these values corresponds to their meaning in the EWS documentation."
+  (let ((text (dom-text dom)))
+    (cond ((string= "Single" text) :single)
+          ((string= "Occurence" text) :occurence)
+          ((string= "Exception" text) :exception)
+          ((string= "RecurringMaster" text) :recurring-master)
+          (t nil))))
 
-(defun org-ews--parse-end-time (node)
-  "Parse NODE and return its ending time.
-Since there is no sane fallback value for this field we return nil upon error instead."
-  (org-ews--parse-simple-text-field (dom-by-tag node 'End) nil))
+(defun org-ews--parse-my-response-type (dom)
+  "Parse DOM and return its text value as a symbol representing the my response type property.
+Possible values for the my response type property include:
+- `:unknown'
+- `:organizer'
+- `:tentative'
+- `:accept'
+- `:decline'
+- `:no-response-received'
+The meaning of these values corresponds to their meaning in the EWS documentation."
+  (let ((text (dom-text dom)))
+    (cond ((string= "Unknown" text) :unknown)
+          ((string= "Organizer" text) :organizer)
+          ((string= "Tentative" text) :tentative)
+          ((string= "Accept" text) :accept)
+          ((string= "Decline" text) :decline)
+          ((string= "NoResponseReceived" text) :no-response-received)
+          (t org-ews-default-my-response-type))))
 
-(defconst org-ews-status-fallback :free
-  "The fallback status for an event that does not have a status or whose status is an invalid value.")
-
-(defun org-ews--parse-legacy-free-busy-status (node)
-  "Parse NODE and return its LegacyFreeBusyStatus property.
-Possible return values are `:free', `:busy', `:no-data',
-`:out-of-office', `:tentative', `:elsewhere'.
-The values correspond to their meaning in the EWS specification.
-If the text value does not fit to any of the above,
-the value specified by `org-ews-status-fallback' is returned."
-  (let ((text (org-ews--parse-simple-text-field (dom-by-tag node 'LegacyFreeBusyStatus) nil)))
-    (cond ((string= text "Free") :free)
-          ((string= text "Busy") :busy)
-          ((string= text "NoData") :no-data)
-          ((string= text "OOF") :out-of-office)
-          ((string= text "Tentative") :tentative)
-          ((string= text "WorkingElsewhere") :elsewhere)
-          (t org-ews-status-fallback))))
-
-(defconst org-ews-location-fallback "Unspecified"
-  "The fallback location value for an event that does not have a location specified.")
-
-(defun org-ews--parse-location (node)
-  "Parse NODE and return its location property."
-  (org-ews--parse-simple-text-field (dom-by-tag node 'Location) org-ews-location-fallback))
-
-(defconst org-ews-item-type-fallback :single
-  "The fallback item type value for an entry that does not have a valid item type.")
-
-(defun org-ews--parse-item-type (node)
-  "Parse NODE and return its calendar item type property.
-Possible return values are `:single', `:occurence', `:exception'
-or `:recurring-master'.
-The values correspond to their meaning in the EWS specification.
-If the text value does not fit to any of the above,
-the value specified by `org-ews-item-type-fallback' is returned."
-  (let ((text (org-ews--parse-simple-text-field (dom-by-tag 'CalendarItemType) nil)))
-    (cond ((string= text "Single") :single)
-          ((string= text "Occurence") :occurence)
-          ((string= text "Exception") :exception)
-          ((string= text "RecurringMaster") :recurring-master)
-          (t org-ews-item-type-fallback))))
-
-(defconst org-ews-my-response-type-fallback :unknown
-  "The fallback MyResponseType value for an event that does not have a valid MyResponseType field.")
-
-(defun org-ews--parse-my-response-type (node)
-  "Parse NODE and return its MyResponseType property."
-  (let ((text (org-ews--parse-simple-text-field (dom-by-tag node 'MyResponseType) nil)))
-    (cond ((string= text "Unknown") :unknown)
-          ((string= text "Organizer") :organizer)
-          ((string= text "Tentative") :tentative)
-          ((string= text "Accept") :accept)
-          ((string= text "Decline") :decline)
-          ((string= text "NoResponseReceived") :no-response-received)
-          (t org-ews-my-response-type-fallback))))
-
-(defun org-ews--parse-routing-type (node)
-  "Parse NODE and return its RoutingType property."
-  (ignore))
-
-(defun org-ews--parse-simple-text-field (node &optional fallback)
-  "Parse NODE and return its text value.
-FALLBACK is returned instead, if either there is no text value
-or the text value is the empty string."
-  (let ((text (dom-text node)))
-        (if (or (not text) (string= "" text)) fallback text)))
-
-(defun org-ews--parse-boolean-text-field (node)
-  "Parse NODE and return its text value as a boolean value.
-A text value of \"true\" is considered t, while other values are considered nil."
-  (string= "true" (dom-text node)))
+(defun org-ews--parse-organizer (dom)
+  "Parse DOM and return its children as an assocation list representing its organizer property.
+")
 
 (provide 'org-ews-parser)
 ;;; org-ews-parser.el ends here
